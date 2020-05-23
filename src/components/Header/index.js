282f0e133a30368ba06b3b8f18d1994ba218f574
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-23 17:39:29 -0300

    criando styles do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 import { Container } from './styles';
 import logo from '../../assets/images/logo.svg'
 
 function Header() {
   return (
-      <Container>
-          <img src={logo} alt="logo"/>
-      </Container>
+    <Container>
+      <img src={logo} alt="logo" />
+    </Container>
   );
 }
 
